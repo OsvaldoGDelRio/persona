ffea468b8c2ccba7eb9fe197eabeb4b81212faca
Osvaldo G Del Rio <osdelrio@hotmail.com>
2021-08-05 14:19:10 -0700

    Clase Persona

---
+++ PrimerApellido.php
@@ -0,0 +1,28 @@
+<?php
+namespace persona;
+use Exception;
+
+Class PrimerApellido
+{
+    private $_apellido;
+
+    public function __construct(string $Apellido)
+    {
+        $this->_apellido = $this->setApellido($Apellido);  
+    }
+
+    public function primerApellido(): string
+    {
+        return $this->_apellido; 
+    }
+
+    private function setApellido($Apellido): string
+    {
+        if(!strlen($Apellido) > 0)
+        {
+            throw new Exception("El primer apellido está vacio");
+        }
+        
+        return $Apellido;
+    }
+}
---
+++ SegundoApellido.php
@@ -0,0 +1,27 @@
+<?php
+namespace persona;
+
+Class SegundoApellido
+{
+    private $_apellido;
+
+    public function __construct(?string $Apellido = null)
+    {
+        $this->_apellido = $this->setApellido($Apellido);  
+    }
+
+    public function segundoApellido(): string
+    {
+        return $this->_apellido; 
+    }
+
+    private function setApellido($Apellido): string
+    {
+        if(!$Apellido)
+        {
+            return '';
+        }
+
+        return $Apellido;
+    }
+}
